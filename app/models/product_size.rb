c4a470aef102b3fcc7a02b3a3626c246858fc4a4
Daniel Lopez <dlopez.10g@gmail.com>
2024-02-19 06:06:51 -0500

    MVC logic for product sizes

--- app/models/product_size.rb
+++ app/models/product_size.rb
@@ -1,4 +1,7 @@
 class ProductSize < ApplicationRecord
   belongs_to :product
   has_one :recipe, dependent: :destroy
+  validates :size, uniqueness: { scope: :product_id, message: "ya existe para este producto" }
+
+  SIZES = ["Personal", "Mediana", "Grande"]
 end
